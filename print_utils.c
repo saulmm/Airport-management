2d451da95e4c9dedfbc82a0e55edbfa17ecc08b6
Saúl <saulmm2@gmail.com>
2014-03-15 17:54:21 +0100

    First commit, main structure and logic

---
+++ print_utils.c
@@ -0,0 +1,50 @@
+/* 
+ * File:   print_utils.c
+ * Author: wtf
+ *
+ * Created on 15 de marzo de 2014, 17:29
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void printSeparator();
+
+void printBox(char* message) {
+    printSeparator();
+    printf("%s", message);
+    printSeparator();
+}
+
+void printSeparator() {
+    printf("\n════════════════════════\n");
+}
+
+
+
+int askForConfirmation (char* message, char* option1, char* option2) {
+    char option;
+  
+    do {
+        printf("%s\n1) %s\n2) %s\n", message, option1, option2);
+        printf("\nIntroduzca su opcion:\n");
+        
+        scanf(" %c", &option);
+
+        switch (option) {
+            case '1':
+                return 1;
+                break;
+                
+            case '2':
+                return 0;
+                break;
+
+            default:
+                printf("Opcion incorrecta\n");
+        }
+    } while (1); 
+    
+    // This never will happen
+    return 0;
+}
